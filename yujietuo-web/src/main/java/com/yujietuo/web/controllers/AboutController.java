a03eb252118f1509e6c925bc8d86913ef302b2d8
Navin <navin.chung@gmail.com>
2018-11-19 01:04:33 +0800

    11-18#02

--- yujietuo-web/src/main/java/com/yujietuo/web/controllers/AboutController.java
+++ yujietuo-web/src/main/java/com/yujietuo/web/controllers/AboutController.java
@@ -1,5 +1,22 @@
 package com.yujietuo.web.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+import java.util.Map;
+
+@Controller
 public class AboutController {
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @GetMapping("/about")
+    public String welcome(Map<String, Object> model) {
+        System.out.println("about ===>");
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "about";
+    }
 }
